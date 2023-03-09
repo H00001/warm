c8d2839ce9bf5a101245ccae9ebd087f0dfd63ae
frank <system-noreply@gunplan.top>
2023-03-09 16:29:07 +0800

    first commit

---
+++ wamr-compiler/build_llvm.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Copyright (C) 2020 Intel Corporation. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+/Library/Developer/CommandLineTools/usr/bin/python3 ../build-scripts/build_llvm.py "$@"
